526621c52329dadce5f751e8cff3543543dce56d
kruzelgo <kruzelgo@gmail.com>
2024-02-11 11:48:58 +0100

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,6 @@
 import Notiflix from 'notiflix';
 
-function createPromise(delay) {
+function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
@@ -27,14 +27,55 @@ form.addEventListener('submit', function (event) {
 
     createPromise(i + 1, currentDelay)
       .then(({ position, delay }) => {
-        Notiflix.Notify.Success(
+        Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
         );
       })
       .catch(({ position, delay }) => {
-        Notiflix.Notify.Failure(
+        Notiflix.Notify.failure(
           `❌ Rejected promise ${position} in ${delay}ms`
         );
       });
   }
 });
+
+// import Notiflix from 'notiflix';
+
+// function createPromise(delay) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const shouldResolve = Math.random() > 0.3;
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
+
+// const form = document.querySelector('.form');
+
+// form.addEventListener('submit', function (event) {
+//   event.preventDefault();
+
+//   const firstDelay = parseInt(form.elements['delay'].value);
+//   const delayStep = parseInt(form.elements['step'].value);
+//   const amount = parseInt(form.elements['amount'].value);
+
+//   for (let i = 0; i < amount; i++) {
+//     const currentDelay = firstDelay + i * delayStep;
+
+//     createPromise(i + 1, currentDelay)
+//       .then(({ position, delay }) => {
+//         Notiflix.Notify.Success(
+//           `✅ Fulfilled promise ${position} in ${delay}ms`
+//         );
+//       })
+//       .catch(({ position, delay }) => {
+//         Notiflix.Notify.Failure(
+//           `❌ Rejected promise ${position} in ${delay}ms`
+//         );
+//       });
+//   }
+// });
